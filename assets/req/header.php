4511b6cda8456c030979449442fbb429e02765da
@sindyko <naykatexnica@gmail.com>
2022-09-26 16:18:34 +0800

    html
    html

---
+++ assets/req/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Ninth navbar example">
+    <div class="container">
+      <a class="navbar-brand" href="#">Container</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample07XL">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Главная</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+        </ul>
+        <div class="text-end dropdown">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown-left">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu dropdown-menu-lg-end">
+              <li><a class="dropdown-item" href="#">Профиль</a></li>
+              <li><a class="dropdown-item" href="#">Настройки</a></li>
+              <hr>
+              <li><a class="dropdown-item" href="#">Выйти</a></li>
+            </ul>
+          </li>
+        </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet"/>
+	<link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
 	<title>Main page</title>
 </head>
 <body>
-	Hello world!
+	<main>
+		<?php require_once 'assets/req/header.php';?>
+	</main>
+
+	<div class="container mb-4">
+    	<p>Matching .container-xl...</p>
+  	</div>
+
+	<script src="assets/js/bootstrap.bundle.js"></script>
 </body>
 </html>
